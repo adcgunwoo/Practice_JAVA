e2e4b530f97fc92cf36c6e6185fe3595799cd05e
김건우 <gun2935@naver.com>
2025-06-29 14:13:17 +0900

    feat : printf 함수 수정 println으로 변경

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
         // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
-        System.out.printf("Hello and wlcome!");
+        System.out.println("Hello and welcome!");
         for (int i=1; i<=5; i++){
             System.out.println("i = " +i); //C와 C++과 다른 특이한 형태를 지닌다.
         }
