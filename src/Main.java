a60558e1dcda870d8528db19b271c896b9366056
김건우 <gun2935@naver.com>
2025-07-03 17:46:30 +0900

    feat : 자바에서의 형변환 명시적 형변환의 형태에 주목하고, 암묵적 형변환의 규칙에 유의하자.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(123+4); // 127출력
-        System.out.println("123"+4); // 1234가 출력된다.
-        //자바에서는 문자열과 숫자를 +로 연결하면 둘 다 문자열로 인식해 두 문자열을 한 던어처럼 붙여서 출력한다.
+        int a = 100;
+        int b = 3;
+        System.out.println((double)a/b); //명시적 형변횐(int를 double로)
+        //(자료형)변수의 형태를 가진다!! 헷갈리지 말것
+
+        //묵시적 형변환
+        //자바 컴파일러가 자동으로 수행하며 범위가 작은 자료형에서 범위가 큰 자료형으로의 변환만 허용!!
+        //byte(1바이트) -> short(2) -> int(4) -> long(8) -> float(4) -> double(8) 점점 커지는 범위
+        //반대로 범위가 큰 자료형에서 범위가 작은 자료형으로 변환할 떄는 반드시 명시적 형변환을 해줘야 한다.
     }
 }
