1a71296efe4d4085a9068dfa62f97a568ade5cf5
김건우 <gun2935@naver.com>
2025-07-04 17:02:38 +0900

    feat : 배열 in 자바 우리가 알던 배열의 형태와 달라졌는데 형태에 유의하고 넘어가겠다.

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        int[] scoreArray;
+        String gradeArray[]; //자바에서는 다음과 같이 배열을 선언할 수 있다.
 
-        int a = scan.nextInt();
+        String[] gradeArray2 = {"A+", "B+", "B+"}; //선언과 동시에 초기화 하는 방법이다. 자료형 뒤에 []기호를 꼭 붙여주자.
 
-        for (int i = 1; i <= a; i++){
-            System.out.print(i);
-        }
-        System.out.println();
-        int j=1;
-        while (j<=a){
-            System.out.print(j++);
-        }
+        int[] scoreArray2 = new int[4]; //이렇게도 배열을 생성할 수 있다.
+        // 크기가 4인 정수형 배열을 선언하고 값을 모두 0으로 초기화할 수 있다.
 
-
-        scan.close();
+        String[] copyArray = gradeArray2; //다음과 같이 배열을 복사할 수 있다. 얕은 복사겠죠?
     }
 }
