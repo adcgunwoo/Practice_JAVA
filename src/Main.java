d506badc4e9e2df10b2b27c60da4de3c40c9ce4b
김건우 <gun2935@naver.com>
2025-07-04 16:30:23 +0900

    feat : 삼항연산자 삼항연산자의 형태는 이해하기 어렵지만, 이해해보자. (조건) ? A : B

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.print("점수를 입력하세요 : ");
-
         int a = scan.nextInt();
-        switch (a){
-            case (100):
-                System.out.println("축하드립니다!");
-                break;
-            default:
-                int b = 100-a;
-                System.out.println("100점에 "+ b +"점이 부족했습니다.");
-        }
+
+        String Grade = (a>=95) ? "Great!" : "SoSo";
+
+        System.out.println(Grade);
 
         scan.close();
-
-
     }
 }
