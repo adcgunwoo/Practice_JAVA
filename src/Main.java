2d3571ccd2c26e91e472d76becfd085d845f9832
김건우 <gun2935@naver.com>
2025-07-10 17:01:25 +0900

    feat : 클래스 in 자바 자바의 클래스 형식에 대해 알아보았다. 용어에 유의하고, C++과 비슷한 점이 많았다.

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,6 @@
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int[] scoreArray;
-        String gradeArray[]; //자바에서는 다음과 같이 배열을 선언할 수 있다.
-
-        String[] gradeArray2 = {"A+", "B+", "B+"}; //선언과 동시에 초기화 하는 방법이다. 자료형 뒤에 []기호를 꼭 붙여주자.
-
-        int[] scoreArray2 = new int[4]; //이렇게도 배열을 생성할 수 있다.
-        // 크기가 4인 정수형 배열을 선언하고 값을 모두 0으로 초기화할 수 있다.
-
-        String[] copyArray = gradeArray2; //다음과 같이 배열을 복사할 수 있다. 얕은 복사겠죠?
-
-        int[] orginArray = {1,2,3}; //깊은 복사를 위한 원본 배열 선언 및 초기화
-        int[] deepCopyArray = new int[orginArray.length]; //깊은 복사를 위한 deepCopyArray 배열을 원본 크기와 똑같이 선언
-        //length라는 인자를 그냥 사용할 수 있네
-
-        for (int i=0; i< orginArray.length; i++){
-            deepCopyArray[i]=orginArray[i]; //일일히 원본 값을 복사해줍니다. 따라서 값만 옮겨가므로 깊은 복사가 이뤄짐
-        }
-
-        deepCopyArray[0]=10;
-        //다음과 같이 복사 배열의 값을 변경해도, 원본 배열의 값은 변하지 않음.
 
     }
 }
---
+++ src/ch07/Car.java
@@ -0,0 +1,38 @@
+package ch07;
+//package는 패키지를 나타내는 키워드이다.
+//ch07은 패키지명(패키지명은 무조건 소문자여야한다!!!)
+
+
+//public은 접근 제한자로 아무것도 붙이지 않거나 public을 붙이는 2가지만 가능(C++에서는 protected 도 있었다)
+//Class는 클래스임을 나태내는 키워드. 뒤 Car은 클래스명이다
+public class Car {
+    static int countOfCars = 0; //static 변수 또는 클래스 변수, 클래스로 생성된 모든 객체가 공유
+
+    String brand; //아래 3개는 인스턴수 변수로, 객체마다 각각 고유한 값이 저장됨
+    int year;
+    String color;
+
+    public void carInfo(){ //클래스 안에 정의한 함수를 메서드라고 한다.
+        //public은 접근제한자로 접근범위를 제한할 수 있다.
+        //반환형과 매개변수를 합쳐 메서드 시그니처라 한다.
+
+        System.out.println("----자동차 정보----");
+        System.out.println("브랜드 : "+ brand);
+        System.out.println("연식 : "+ year);
+        System.out.println("색 : "+ color);
+        //다음과 같이 인스턴스 변수에 접근할 수 있는 메서드를 인스턴스 메서드라고 한다.
+    }
+    public static void countOfCarsInfo(){
+        System.out.println("자동차 수량 : "+countOfCars);
+        //클래스 변수에 접근하였다. 이처럼 클래스 변수에 접근할 수 있는 메서드를 클래스 메서드라고 한다.
+        //static 키워드를 사용했기에 정적 메서드라고도 한다.
+    }
+    //중요!!!!!!!!! 클래스 메서드는 인스턴스 변수나 인스턴스 메서드에 접근할 수 없다!!
+
+    public Car(){
+        System.out.println("새로운 자동차 객체가 생송됐습니다.");
+    }
+    //위 생성자는 기본생성자로서 굳이 정의하지 않아도 컴파일러가 자동으로 추가한다.
+    //생성자는 여러 개 정의할수 있고, 매개변수가 전부 달라야 한다.
+
+}
