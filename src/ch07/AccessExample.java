b5010436ea2e0b135a4b955e5590375aa2a8e7aa
김건우 <gun2935@naver.com>
2025-07-13 16:30:45 +0900

    feat : 접근 제한자 in 자바 접근 제한자 차이에 따른 같은 패키지속 클래스의 접근 권한이 달라지는 것을 확인할 수 있었다.

--- src/ch07/AccessExample.java
+++ src/ch07/AccessExample.java
@@ -3,13 +3,18 @@ package ch07;
 //public : 같은 패키지든 다른 패키지든 상관없이 어디서든 접근 가능하다. 클래스, 필드, 메서드, 생성자에 모두 사용할 수 있다.
 //protected : 같은 패키지의 클래스나 다른 패키지의 상속받은 클래스에서 접근할 수 있다. 클래스에는 사용할 수 없고, 필드, 메서드, 생성자에 사용할 수 있다.
 //privated : 같은 클래스에서만 접근할 수 있다. protected와 마찬가지로 필드, 메서드, 생성자에만 사용할 수 있다.
-//default : 접근 제한자를 따로 명시하지 않으면 자동으로 default 권한이 주어지고, 같은 패키지의 클래스에서만 접근 가능하고, 클래스, 필드, 메서드, 생성자 모두 가능하다.
-public class Main {
-    public static void main(String[] args) {
-        AccessExample ae = new AccessExample();
-        ae.publicMethod(); //public이기에 접근 가능
-        ae.protectedMethod(); //같은 패키지의 클래스이기에 접근이 가능함
-        ae.defaultMethod();
-        ae.privatedMethod();
+
+public class AccessExample {
+    public void publicMethod(){
+        System.out.println("public 메서드");
+    }
+    protected void protectedMethod(){
+        System.out.println("protected 메서드");
+    }
+    void defaultMethod(){
+        System.out.println("default 메서드");
+    }
+    private void privateMethod(){
+        System.out.println("private 메서드");
     }
 }
