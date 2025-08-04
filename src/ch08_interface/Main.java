15cec769e7566fb3d22448a1928b2f89d37cd932
김건우 <gun2935@naver.com>
2025-08-04 15:53:41 +0900

    feat : 인터페이스 추가 개념 주석처리된 내용이외로 인터페이스는 추상 클래스와 마찬가지로 직접 객체를 생성할 수는 없다. 구현 클래스에서 객체를 생성하고 객체에서 인터페이스를 자료형으로 선언할 수는 있다. implements 키워드로 구현할 인터페이스를 명시한다. 이미 다른 클래스를 상속받은 상태라면 extends 부모클래스명 implements 인터페이스1, 인터페이스2 처럼 뒤에 추가해준다. 여러개 라면 ,로 나열한다. 또한 인터페이스끼리 상속할 때는 extends 키워드를 사용한다.

--- src/ch08_interface/Main.java
+++ src/ch08_interface/Main.java
@@ -1,5 +1,11 @@
 package ch08_interface;
-
+//ps) 인터페이스 에서의 디폴트 메서드와 정적 메서드
+//디폴트 메서드는 메서드 동작이 구현된 메서드로, 메서드 앞에 default 키워드가 붙는다. default 키워드가 붙지않으면 추상메서드로 인식
+//인터페이스를 구현한 클래스에서 오버라이딩할 수 있다. 디폴트 메서드를 사용하면 구현 클래스의 기존 구조와 동작을 망가뜨리지 않으면서
+//새 기능을 추가할 수 있다.
+//정적 메서드는 앞에 static 키워드가 붙으며 인터페이스 이름으로 직접 호출할 수 있다. 디폴트 메서드처럼 동작이 구현돼 있지만, 구현 클래스에서
+//오버라이딩 할 수는 없다.
+//인터페이스는 또한 상수만 포함할 수 있다. 인터페이스에 필드를 선언하면 자동으로 public, static, final 키워드가 붙는다.
 public class Main {
     public static void main(String[] args){
         CheckingAccount myChecking = new CheckingAccount("123-4567890", 10000,0.02);
