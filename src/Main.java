4cbfd41df7d71147a3c23daaf231c01710793490
김건우 <gun2935@naver.com>
2025-08-10 16:24:28 +0900

    feat : 예외처리(throw로 예외 고의로 발생시키기) 개발자는 예외를 고의로 발생시킬 수 있다. throw는 줋 RuntimeException 예외를 처리할 때 사용한다. 형식은 new 키워드로 지정한 예외 클래스의 객체를 생성하고 앞에 throw 키워드를 붙여 예외를 발생시킨다. 이때 예외 메시지를 작성하면 예외 발생 시 해당 메시지가 출력한다.

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("첫 번째 정수를 입력하세요.--> ");
+        System.out.println("첫번째 정수를 입력하세요. -->");
         int a = scan.nextInt();
-        System.out.println("두 번째 정수를 입력하세요.--> ");
-        int b = scan.nextInt(); //b(분모)에 0 입력
-        try{
-            int result = a / b;
-            System.out.println("결과 : " + result);
+        System.out.println("두번째 정수를 입력하세요. -->");
+        int b = scan.nextInt();
+        try {
+            if(b==3){ //3으로 나누려 할때 고의로 예외를 던짐
+                throw new ArithmeticException("3으로 나눌 수 없습니다.");
+                //new 키워드로 ArithmeticException 클래스의 객체를 생성
+                //이 객체를 catch로 던짐
+            }
+            int result=a/b;
+            System.out.println("결과: "+result);
         }
         catch (ArithmeticException e){
-            System.out.println(e);
+            System.out.println(e.getMessage());
+            //getMessage() 메서드는 Throwable 클래스에 속한 메서드로, 예외가 발생할 때 표시하는 예외 메시지에(by zero)를 문자열로 반환
+            //예외 객체를 생성할 때 예외 메시지를 지정해 전달하면 getMessage() 메서드로 반환할 메시지를 직접 지정할 수 있다.
         }
         finally {
             System.out.println("프로그램을 종료합니다.");
         }
     }
 }
-// tty-catch-finally 문으로 예외처리하기 만약 하지 않았다면?
-// ArithmeticException 예외 발생, 왜? 밑에 0이 들어갔기 때문에
