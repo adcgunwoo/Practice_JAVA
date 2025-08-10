6313d61f2f2d81459c7f638daf2de818a821aeb9
김건우 <gun2935@naver.com>
2025-08-10 15:09:14 +0900

    feat : 예외처리(try-catch-finally문) 분모에 0을 넣고 나누기 연산을 하지 못하는 것은 모두가 알고 있다. 이를 컴파일러가 실행 시 ArithmeticException을 일으키는데 예외 처리로 예외 정보를 e 객체에 저장하고, 예외 객체는 catch 블록으로 보낸다. try에서 보낸 예외 객체가 catch 블록에 정의된 예외와 일치하면 catch 블록 안에 있는 내용을 수행한다.(출력내용을 보면 예외 처리하지 않았을 때 나온 오류 메시지와 같다.)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        Scanner scan = new Scanner(System.in);
+        System.out.println("첫 번째 정수를 입력하세요.--> ");
+        int a = scan.nextInt();
+        System.out.println("두 번째 정수를 입력하세요.--> ");
+        int b = scan.nextInt(); //b(분모)에 0 입력
+        try{
+            int result = a / b;
+            System.out.println("결과 : " + result);
+        }
+        catch (ArithmeticException e){
+            System.out.println(e);
+        }
+        finally {
+            System.out.println("프로그램을 종료합니다.");
+        }
     }
 }
+// tty-catch-finally 문으로 예외처리하기 만약 하지 않았다면?
+// ArithmeticException 예외 발생, 왜? 밑에 0이 들어갔기 때문에
